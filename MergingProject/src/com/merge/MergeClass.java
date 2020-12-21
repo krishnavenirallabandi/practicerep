6c0206d1e91e3db19836adbf304dc4c293081830
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2020-12-20 22:59:33 -0800

    MergingProject added

---
+++ MergingProject/src/com/merge/MergeClass.java
@@ -0,0 +1,14 @@
+package com.merge;
+
+public class MergeClass {
+
+	public static void main(String[] args) {
+		//Addition
+		int a=5,b=10;
+		int sum=0;
+		sum=a+b;
+		System.out.println("addition of a and b is    "+sum);
+		
+	}
+
+}
